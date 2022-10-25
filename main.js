12e4eacc447fa31e4b54462b55278d21c4e4272e
HoreyVer <artjom.bodroff@yandex.ru>
2022-10-25 22:39:36 +0300

    commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function expressionMatter(a, b, c) {
+	let max = 0;
+	let a1 = a * (b + c);
+	let a2 = a * b * c;
+	let a3 = a + b * c;
+	let a4 = (a + b) * c;
+	let a5 = a + b + c;
+	if (a1 >= max) {
+		max = a1;
+	}
+	if (a2 >= max) {
+		max = a2;
+	}
+	if (a3 >= max) {
+		max = a3;
+	}
+	if (a4 >= max) {
+		max = a4;
+	}
+	if (a5 >= max) {
+		max = a5;
+	}
+	return max;// highest achievable result
+}
